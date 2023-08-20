31b381b1c5fff413bc41cc1401c92442f514acfd
Lahori Jawan <nasir.khan@tkxel.com>
2023-08-20 14:00:20 +0500

    maja agaya na

--- Project-2/script.js
+++ Project-2/script.js
@@ -1,3 +1,6 @@
+// Interactive Button:
+// Create a button. When clicked, it should display an alert with the message
+// "Button was clicked!".
 const button = document.querySelector("button");
 
 button.addEventListener("click", function () {
