2bbca46d81c7307160e79d87bc050798e745a357
Lahori Jawan <nasir.khan@tkxel.com>
2023-08-20 13:59:25 +0500

    maja agaya

--- Project-1/script.js
+++ Project-1/script.js
@@ -1,8 +1,17 @@
+// const buttons = document.querySelectorAll("button");
+
+// buttons.forEach(function (button) {
+//   button.addEventListener("click", function () {
+//     const output = window.getComputedStyle(button).background;
+//     document.body.style.background = output;
+//   });
+// });
+
 const buttons = document.querySelectorAll("button");
 
 buttons.forEach(function (button) {
   button.addEventListener("click", function () {
-    const output = window.getComputedStyle(button).background;
+    const output = getComputedStyle(button).background;
     document.body.style.background = output;
   });
 });
---
+++ Project-2/script.js
@@ -0,0 +1,5 @@
+const button = document.querySelector("button");
+
+button.addEventListener("click", function () {
+  prompt("Arrrest Him", "Button was clicked!");
+});
